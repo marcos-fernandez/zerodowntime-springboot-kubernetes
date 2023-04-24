9ad52a8dff0c3c7fd8485b96e955fd3b8b9448b4
marcos.fernandez <marcos.fernandez@avoristravel.com>
2023-04-24 06:44:07 +0200

    - Initial commit. App generated from start.spring.io

---
+++ src/test/java/com/avoristravel/zerodowntime/ZerodowntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avoristravel.zerodowntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodowntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
